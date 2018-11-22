2e02414282d8928d0a36104ddbcf7cb35f40ffb1
conted <conted@BH210-17.studentservers.JAC-SERVERS.JOHNABBOTT.QC.CA>
2018-11-21 20:47:00 -0500

    lecture2 pythonII

---
+++ modules.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 19:03:19 2018
+
+@author: conted
+"""
+
+import time
+a =time.time()
+#for i in range(8000000):
+#    i+=i
+print(time.time()-a)    
+print(time.asctime())
